85858e9a9188b8867be85cd51423591e61c5fdab
vienyevelyn <vieny.evelin@gmail.com>
2026-06-06 02:07:28 +0700

    week08: create nested classes for Order

---
+++ src/week8/OrderModels.kt
@@ -0,0 +1,6 @@
+package week8
+
+class City(val name: String)
+class Address(val city: City?)
+class DeliveryDetails(val address: Address?)
+class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)
