04b42d7ede871a93e676eef815deaf3af5e94922
vienyevelyn <vieny.evelin@gmail.com>
2026-06-06 02:16:49 +0700

    week08: implement safe casting as? to filter collection

--- src/week8/Main.kt
+++ src/week8/Main.kt
@@ -6,20 +6,26 @@ fun main(){
     val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "Kota tidak ada"
     println("destination = ${destination}")
 
-    println("=== TEST LET BLOCKS ===")
+    println("\n=== TEST LET BLOCKS ===")
     val validOrder = Order(null, 250000)
     val receipt = validOrder.totalPrice?.let {
         price -> val tax = price *0.11
         "Transaksi valid. Harga : Rp$price, tax: $tax"
     } ?: "Transaksi Invalid"
 
-    println("=== TEST SAFE CASTING ===")
+    println("\n=== TEST SAFE CASTING ===")
     val mixedData : List<Any> = listOf(
         "Smartphone",
         1500000,
-        UserProfile("Andi", null)
+        UserProfile("Andi", null),
         "Laptop",
         450000.0
     )
 
+    for(i in mixedData){
+        val text = i as? String
+        text?.let {
+            println(it.uppercase())
+        }
+    }
 }
